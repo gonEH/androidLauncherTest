a37639173e3f28c604a7644c2132cf92a57abb75
gontai Kim <xellossg@nate.com>
2019-04-03 12:18:16 +0900

    * minSdkVersion 9에도 빌드 되도록 수정: targetSdkVersion을 25이하로 해야함 * Home button을 누를시 앱을 실행 : <category android:name="android.intent.category.HOME"/> * Back button 막음

--- app/src/main/java/com/goneh/androidlaunchertest/MainActivity.kt
+++ app/src/main/java/com/goneh/androidlaunchertest/MainActivity.kt
@@ -2,11 +2,16 @@ package com.goneh.androidlaunchertest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return false
     }
 }
